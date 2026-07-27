616e94f25eb9b9926f667ce5bff8f9ebdaf5ee80
FRodrigues <frodrigues.gaia@gmail.com>
2026-07-27 11:48:28 +0100

    Add container-job-assignment Livewire component
    Create a new Livewire component for job assignment.

---
+++ container-job-assignment.blade.php
@@ -0,0 +1,13 @@
+<?php
+use Livewire\Component;
+use Illuminate\Support\Str;
+use App\DTO\GridDefinition;
+use App\DTO\GridColumnDefinition;
+use App\Models\WarehouseSections;
+
+new class extends Component
+{
+//add your code
+  
+};
+?>
